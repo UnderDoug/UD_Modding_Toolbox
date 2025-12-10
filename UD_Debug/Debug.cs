19292f5cd5999cfd5ed3ced46ca2505ae7e9a5f6
UnderDoug <douglas.marken@gmail.com>
2025-12-11 08:57:38 +1000

    Temporarily disabled the obsoletion messaging about debug-logging.

--- UD_Debug/Debug.cs
+++ UD_Debug/Debug.cs
@@ -37,10 +37,10 @@ namespace UD_Modding_Toolbox
 
         private static int _LastIndent = 0;
 
-        [Obsolete(
-            "Prefer " + nameof(Debug) + "." + nameof(GetIndent) + " or " + 
-            nameof(UD_Modding_Toolbox.UD_Logger) + "." + nameof(UD_Modding_Toolbox.UD_Logger.GetIndent) + 
-            "; this property will persist for a while (into 2026).")]
+        // [Obsolete(
+        //     "Prefer " + nameof(Debug) + "." + nameof(GetIndent) + " or " + 
+        //     nameof(UD_Modding_Toolbox.UD_Logger) + "." + nameof(UD_Modding_Toolbox.UD_Logger.GetIndent) + 
+        //     "; this property will persist for a while (into 2026).")]
         public static int LastIndent
         {
             get
@@ -72,149 +72,149 @@ namespace UD_Modding_Toolbox
             Logger.SetIndent(Indent);
         }
 
-        [Obsolete("Prefer " + nameof(UD_Modding_Toolbox.UD_Logger) + "." + nameof(UD_Modding_Toolbox.UD_Logger.Entry) + "; this method will persist for a while (into 2026).")]
+        // [Obsolete("Prefer " + nameof(UD_Modding_Toolbox.UD_Logger) + "." + nameof(UD_Modding_Toolbox.UD_Logger.Entry) + "; this method will persist for a while (into 2026).")]
         public static void Entry(int Verbosity, string Text, int Indent = 0, bool Toggle = true)
         {
             Entry((UD_Logger.Verbosity)Verbosity, Text, Indent, Toggle: Toggle);
         }
 
-        [Obsolete("Prefer " + nameof(UD_Modding_Toolbox.UD_Logger) + "." + nameof(UD_Modding_Toolbox.UD_Logger.Entry) + "; this method will persist for a while (into 2026).")]
+        // [Obsolete("Prefer " + nameof(UD_Modding_Toolbox.UD_Logger) + "." + nameof(UD_Modding_Toolbox.UD_Logger.Entry) + "; this method will persist for a while (into 2026).")]
         public static void Entry(UD_Logger.Verbosity Verbosity, string Text, int Indent = 0, bool Toggle = true)
         {
             Logger.Entry(Verbosity, Text, Indent, Toggle: Toggle);
         }
 
-        [Obsolete("Prefer " + nameof(UD_Modding_Toolbox.UD_Logger) + "." + nameof(UD_Modding_Toolbox.UD_Logger.Entry) + "; this method will persist for a while (into 2026).")]
+        // [Obsolete("Prefer " + nameof(UD_Modding_Toolbox.UD_Logger) + "." + nameof(UD_Modding_Toolbox.UD_Logger.Entry) + "; this method will persist for a while (into 2026).")]
         public static void Entry(string Text, int Indent = 0, bool Toggle = true)
         {
             Logger.Entry(Text, Indent, Toggle: Toggle);
         }
 
-        [Obsolete("Prefer " + nameof(UD_Modding_Toolbox.UD_Logger) + "." + nameof(UD_Modding_Toolbox.UD_Logger.Entry) + "; this method will persist for a while (into 2026).")]
+        // [Obsolete("Prefer " + nameof(UD_Modding_Toolbox.UD_Logger) + "." + nameof(UD_Modding_Toolbox.UD_Logger.Entry) + "; this method will persist for a while (into 2026).")]
         public static void Entry(int Verbosity, string Label, string Text, int Indent = 0, bool Toggle = true)
         {
             Entry((UD_Logger.Verbosity)Verbosity, Label, Text, Indent, Toggle: Toggle);
         }
 
-        [Obsolete("Prefer " + nameof(UD_Modding_Toolbox.UD_Logger) + "." + nameof(UD_Modding_Toolbox.UD_Logger.Entry) + "; this method will persist for a while (into 2026).")]
+        // [Obsolete("Prefer " + nameof(UD_Modding_Toolbox.UD_Logger) + "." + nameof(UD_Modding_Toolbox.UD_Logger.Entry) + "; this method will persist for a while (into 2026).")]
         public static void Entry(UD_Logger.Verbosity Verbosity, string Label, string Text, int Indent = 0, bool Toggle = true)
         {
             string output = Label + ": " + Text;
             Logger.Entry(Verbosity, output, Indent, Toggle: Toggle);
         }
 
-        [Obsolete("Prefer " + nameof(UD_Modding_Toolbox.UD_Logger) + "." + nameof(UD_Modding_Toolbox.UD_Logger.Divider) + "; this method will persist for a while (into 2026).")]
+        // [Obsolete("Prefer " + nameof(UD_Modding_Toolbox.UD_Logger) + "." + nameof(UD_Modding_Toolbox.UD_Logger.Divider) + "; this method will persist for a while (into 2026).")]
         public static void Divider(int Verbosity = 0, string String = null, int Count = 60, int Indent = 0, bool Toggle = true)
         {
             Divider((UD_Logger.Verbosity)Verbosity, String, Count, Indent, Toggle);
         }
 
-        [Obsolete("Prefer " + nameof(UD_Modding_Toolbox.UD_Logger) + "." + nameof(UD_Modding_Toolbox.UD_Logger.Divider) + "; this method will persist for a while (into 2026).")]
+        // [Obsolete("Prefer " + nameof(UD_Modding_Toolbox.UD_Logger) + "." + nameof(UD_Modding_Toolbox.UD_Logger.Divider) + "; this method will persist for a while (into 2026).")]
         public static void Divider(UD_Logger.Verbosity Verbosity = 0, string String = null, int Count = 60, int Indent = 0, bool Toggle = true)
         {
             Logger.Divider(Verbosity, String, Count, Indent, Toggle);
         }
 
-        [Obsolete("Prefer " + nameof(UD_Modding_Toolbox.UD_Logger) + "." + nameof(UD_Modding_Toolbox.UD_Logger.Header) + "; this method will persist for a while (into 2026).")]
+        // [Obsolete("Prefer " + nameof(UD_Modding_Toolbox.UD_Logger) + "." + nameof(UD_Modding_Toolbox.UD_Logger.Header) + "; this method will persist for a while (into 2026).")]
         public static void Header(int Verbosity, string ClassName, string MethodName, bool Toggle = true)
         {
             Header((UD_Logger.Verbosity)Verbosity, ClassName, MethodName, Toggle: Toggle);
         }
-        [Obsolete("Prefer " + nameof(UD_Modding_Toolbox.UD_Logger) + "." + nameof(UD_Modding_Toolbox.UD_Logger.Header) + "; this method will persist for a while (into 2026).")]
+        // [Obsolete("Prefer " + nameof(UD_Modding_Toolbox.UD_Logger) + "." + nameof(UD_Modding_Toolbox.UD_Logger.Header) + "; this method will persist for a while (into 2026).")]
         public static void Header(UD_Logger.Verbosity Verbosity, string ClassName, string MethodName, bool Toggle = true)
         {
             Logger.Header(Verbosity, ClassName, MethodName, Toggle: Toggle);
         }
 
-        [Obsolete("Prefer " + nameof(UD_Modding_Toolbox.UD_Logger) + "." + nameof(UD_Modding_Toolbox.UD_Logger.Footer) + "; this method will persist for a while (into 2026).")]
+        // [Obsolete("Prefer " + nameof(UD_Modding_Toolbox.UD_Logger) + "." + nameof(UD_Modding_Toolbox.UD_Logger.Footer) + "; this method will persist for a while (into 2026).")]
         public static void Footer(int Verbosity, string ClassName, string MethodName, bool Toggle = true)
         {
             Footer((UD_Logger.Verbosity)Verbosity, ClassName, MethodName, Toggle: Toggle);
         }
-        [Obsolete("Prefer " + nameof(UD_Modding_Toolbox.UD_Logger) + "." + nameof(UD_Modding_Toolbox.UD_Logger.Footer) + "; this method will persist for a while (into 2026).")]
+        // [Obsolete("Prefer " + nameof(UD_Modding_Toolbox.UD_Logger) + "." + nameof(UD_Modding_Toolbox.UD_Logger.Footer) + "; this method will persist for a while (into 2026).")]
         public static void Footer(UD_Logger.Verbosity Verbosity, string ClassName, string MethodName, bool Toggle = true)
         {
             Logger.Footer(Verbosity, ClassName, MethodName, Toggle: Toggle);
         }
 
-        [Obsolete("Prefer " + nameof(UD_Modding_Toolbox.UD_Logger) + "." + nameof(UD_Modding_Toolbox.UD_Logger.DiveIn) + "; this method will persist for a while (into 2026).")]
+        // [Obsolete("Prefer " + nameof(UD_Modding_Toolbox.UD_Logger) + "." + nameof(UD_Modding_Toolbox.UD_Logger.DiveIn) + "; this method will persist for a while (into 2026).")]
         public static void DiveIn(int Verbosity, string Text, int Indent = 0, bool Toggle = true)
         {
             DiveIn((UD_Logger.Verbosity)Verbosity, Text, Indent, Toggle: Toggle);
         }
-        [Obsolete("Prefer " + nameof(UD_Modding_Toolbox.UD_Logger) + "." + nameof(UD_Modding_Toolbox.UD_Logger.DiveIn) + "; this method will persist for a while (into 2026).")]
+        // [Obsolete("Prefer " + nameof(UD_Modding_Toolbox.UD_Logger) + "." + nameof(UD_Modding_Toolbox.UD_Logger.DiveIn) + "; this method will persist for a while (into 2026).")]
         public static void DiveIn(UD_Logger.Verbosity Verbosity, string Text, int Indent = 0, bool Toggle = true)
         {
             Logger.DiveIn(Verbosity, Text, Indent, Toggle: Toggle);
         }
-        [Obsolete("Prefer " + nameof(UD_Modding_Toolbox.UD_Logger) + "." + nameof(UD_Modding_Toolbox.UD_Logger.DiveOut) + "; this method will persist for a while (into 2026).")]
+        // [Obsolete("Prefer " + nameof(UD_Modding_Toolbox.UD_Logger) + "." + nameof(UD_Modding_Toolbox.UD_Logger.DiveOut) + "; this method will persist for a while (into 2026).")]
         public static void DiveOut(int Verbosity, string Text, int Indent = 0, bool Toggle = true)
         {
             DiveOut((UD_Logger.Verbosity)Verbosity, Text, Indent, Toggle: Toggle);
         }
-        [Obsolete("Prefer " + nameof(UD_Modding_Toolbox.UD_Logger) + "." + nameof(UD_Modding_Toolbox.UD_Logger.DiveOut) + "; this method will persist for a while (into 2026).")]
+        // [Obsolete("Prefer " + nameof(UD_Modding_Toolbox.UD_Logger) + "." + nameof(UD_Modding_Toolbox.UD_Logger.DiveOut) + "; this method will persist for a while (into 2026).")]
         public static void DiveOut(UD_Logger.Verbosity Verbosity, string Text, int Indent = 0, bool Toggle = true)
         {
             Logger.DiveOut(Verbosity, Text, Indent, Toggle: Toggle);
         }
 
-        [Obsolete("Prefer " + nameof(UD_Modding_Toolbox.UD_Logger) + "." + nameof(UD_Modding_Toolbox.UD_Logger.Warn) + "; this method will persist for a while (into 2026).")]
+        // [Obsolete("Prefer " + nameof(UD_Modding_Toolbox.UD_Logger) + "." + nameof(UD_Modding_Toolbox.UD_Logger.Warn) + "; this method will persist for a while (into 2026).")]
         public static void Warn(int Verbosity, string ClassName, string MethodName, string Issue = null, int Indent = 0)
         {
             Warn((UD_Logger.Verbosity)Verbosity, ClassName, MethodName, Issue, Indent);
         }
-        [Obsolete("Prefer " + nameof(UD_Modding_Toolbox.UD_Logger) + "." + nameof(UD_Modding_Toolbox.UD_Logger.Warn) + "; this method will persist for a while (into 2026).")]
+        // [Obsolete("Prefer " + nameof(UD_Modding_Toolbox.UD_Logger) + "." + nameof(UD_Modding_Toolbox.UD_Logger.Warn) + "; this method will persist for a while (into 2026).")]
         public static void Warn(UD_Logger.Verbosity Verbosity, string ClassName, string MethodName, string Issue = null, int Indent = 0)
         {
             Logger.Warn(Verbosity, ClassName, MethodName, Issue, Indent);
         }
 
-        [Obsolete("Prefer " + nameof(UD_Modding_Toolbox.UD_Logger) + "." + nameof(UD_Modding_Toolbox.UD_Logger.LoopItem) + "; this method will persist for a while (into 2026).")]
+        // [Obsolete("Prefer " + nameof(UD_Modding_Toolbox.UD_Logger) + "." + nameof(UD_Modding_Toolbox.UD_Logger.LoopItem) + "; this method will persist for a while (into 2026).")]
         public static void LoopItem(int Verbosity, string Label, string Text = "", int Indent = 0, bool? Good = null, bool Toggle = true)
         {
             LoopItem((UD_Logger.Verbosity)Verbosity, Label, Text, Indent, Good, Toggle: Toggle);
         }
-        [Obsolete("Prefer " + nameof(UD_Modding_Toolbox.UD_Logger) + "." + nameof(UD_Modding_Toolbox.UD_Logger.LoopItem) + "; this method will persist for a while (into 2026).")]
+        // [Obsolete("Prefer " + nameof(UD_Modding_Toolbox.UD_Logger) + "." + nameof(UD_Modding_Toolbox.UD_Logger.LoopItem) + "; this method will persist for a while (into 2026).")]
         public static void LoopItem(UD_Logger.Verbosity Verbosity, string Label, string Text = "", int Indent = 0, bool? Good = null, bool Toggle = true)
         {
             Logger.LoopItem(Verbosity, Label, Text, Indent, Good, Toggle: Toggle);
         }
 
-        [Obsolete("Prefer " + nameof(UD_Modding_Toolbox.UD_Logger) + "." + nameof(UD_Modding_Toolbox.UD_Logger.CheckYeh) + "; this method will persist for a while (into 2026).")]
+        // [Obsolete("Prefer " + nameof(UD_Modding_Toolbox.UD_Logger) + "." + nameof(UD_Modding_Toolbox.UD_Logger.CheckYeh) + "; this method will persist for a while (into 2026).")]
         public static void CheckYeh(int Verbosity, string Label, string Text = "", int Indent = 0, bool? Good = true, bool Toggle = true)
         {
             CheckYeh((UD_Logger.Verbosity)Verbosity, Label, Text, Indent, Good, Toggle: Toggle);
         }
-        [Obsolete("Prefer " + nameof(UD_Modding_Toolbox.UD_Logger) + "." + nameof(UD_Modding_Toolbox.UD_Logger.CheckYeh) + "; this method will persist for a while (into 2026).")]
+        // [Obsolete("Prefer " + nameof(UD_Modding_Toolbox.UD_Logger) + "." + nameof(UD_Modding_Toolbox.UD_Logger.CheckYeh) + "; this method will persist for a while (into 2026).")]
         public static void CheckYeh(UD_Logger.Verbosity Verbosity, string Label, string Text = "", int Indent = 0, bool? Good = true, bool Toggle = true)
         {
             Logger.CheckYeh(Verbosity, Label, Text, Indent, Good, Toggle: Toggle);
         }
 
-        [Obsolete("Prefer " + nameof(UD_Modding_Toolbox.UD_Logger) + "." + nameof(UD_Modding_Toolbox.UD_Logger.CheckNah) + "; this method will persist for a while (into 2026).")]
+        // [Obsolete("Prefer " + nameof(UD_Modding_Toolbox.UD_Logger) + "." + nameof(UD_Modding_Toolbox.UD_Logger.CheckNah) + "; this method will persist for a while (into 2026).")]
         public static void CheckNah(int Verbosity, string Label, string Text = "", int Indent = 0, bool? Good = false, bool Toggle = true)
         {
             CheckNah((UD_Logger.Verbosity)Verbosity, Label, Text, Indent, Good, Toggle: Toggle);
         }
-        [Obsolete("Prefer " + nameof(UD_Modding_Toolbox.UD_Logger) + "." + nameof(UD_Modding_Toolbox.UD_Logger.CheckNah) + "; this method will persist for a while (into 2026).")]
+        // [Obsolete("Prefer " + nameof(UD_Modding_Toolbox.UD_Logger) + "." + nameof(UD_Modding_Toolbox.UD_Logger.CheckNah) + "; this method will persist for a while (into 2026).")]
         public static void CheckNah(UD_Logger.Verbosity Verbosity, string Label, string Text = "", int Indent = 0, bool? Good = false, bool Toggle = true)
         {
             Logger.CheckNah(Verbosity, Label, Text, Indent, Good, Toggle: Toggle);
         }
 
         // Class Specific Debugs
-        [Obsolete("Prefer " + nameof(UD_Modding_Toolbox.UD_Logger) + "." + nameof(UD_Modding_Toolbox.UD_Logger.CheckNah) + "; this method will persist for a while (into 2026).")]
+        // [Obsolete("Prefer " + nameof(UD_Modding_Toolbox.UD_Logger) + "." + nameof(UD_Modding_Toolbox.UD_Logger.CheckNah) + "; this method will persist for a while (into 2026).")]
         public static void Vomit(int Verbosity, string Source, string Context = null, int Indent = 0, bool Toggle = true)
         {
             Vomit((UD_Logger.Verbosity)Verbosity, Source, Context, Indent, Toggle: Toggle);
         }
-        [Obsolete("Prefer " + nameof(UD_Modding_Toolbox.UD_Logger) + "." + nameof(UD_Modding_Toolbox.UD_Logger.CheckNah) + "; this method will persist for a while (into 2026).")]
+        // [Obsolete("Prefer " + nameof(UD_Modding_Toolbox.UD_Logger) + "." + nameof(UD_Modding_Toolbox.UD_Logger.CheckNah) + "; this method will persist for a while (into 2026).")]
         public static void Vomit(UD_Logger.Verbosity Verbosity, string Source, string Context = null, int Indent = 0, bool Toggle = true)
         {
             Logger.Vomit(Verbosity, Source, Context, Indent, Toggle: Toggle);
         }
 
-        [Obsolete("Prefer version using " + nameof(UD_Modding_Toolbox.UD_Logger) + "." + nameof(UD_Modding_Toolbox.UD_Logger.Verbosity) + "; this method will persist for a while (into 2026).")]
+        // [Obsolete("Prefer version using " + nameof(UD_Modding_Toolbox.UD_Logger) + "." + nameof(UD_Modding_Toolbox.UD_Logger.Verbosity) + "; this method will persist for a while (into 2026).")]
         public static Raffle<T> Vomit<T>(
             this Raffle<T> Raffle,
             int Verbosity,
